5093a7f15a329f6ac4a12e6acdee0748797c0e34
jgchenu <jgchenu@foxmail.com>
2018-07-09 22:41:56 +0800

    链接数据库

--- service/database/init.js
+++ service/database/init.js
@@ -1,6 +1,18 @@
-const mongoose =require('mongoose')
-const db='mongodb://localhost/smile-db'
-exports.connect=()=>{
-    //链接数据库
-    mongoose.connect(db)
-}
+const mongoose = require("mongoose");
+const db = "mongodb://localhost/smile-db";
+exports.connect = () => {
+  //链接数据库
+  mongoose.connect(db);
+  //增加数据库监听事件
+  mongoose.connection.on("disconnected", () => {
+    console.log("[*******数据库断开]");
+    mongoose.connect(db);
+  });
+  mongoose.connection.on("error", () => {
+    console.log("[**********数据库错误]");
+    mongoose.connect(db);
+  });
+    mongoose.connection.once("open", () => {
+        console.log("[**********mongodb connected successfully]");
+    });
+};
--- service/index.js
+++ service/index.js
@@ -1,5 +1,9 @@
 const Koa=require('koa');
 const app=new Koa;
+const {connect}=require('./database/init.js')
+;(async()=>{
+    await connect()
+})()
 app.use(async(ctx)=>{
     ctx.body=`<h1>Hello world</h1>`
 })
