f817e82bb35a2775f55b84a4f033966d7a353dc3
jgchenu <jgchenu@foxmail.com>
2018-07-11 23:48:16 +0800

    加盐加密

--- service/database/schema/User.js
+++ service/database/schema/User.js
@@ -1,6 +1,8 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 let ObjectId = Schema.Types.ObjectId;
+const bcrypt = require("bcryptjs");
+const SALT_WORK_FACTOR = 10;
 //创建userschema
 const userSchema = new Schema({
   UserId: {
@@ -22,10 +24,22 @@ const userSchema = new Schema({
     default: Date.now()
   }
 }, {
-  collection: 'user'
-}) //加盐
-
+  collection: "user"
+});
+//加盐
+userSchema.pre("save", function (next) {
+  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      this.password = hash;
+      next();
+    });
+  });
+});
 //发布模型
-mongoose.model(
-  'User', userSchema
-)
+mongoose.model("User", userSchema);
