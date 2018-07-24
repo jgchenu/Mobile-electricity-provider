b9a2cbd3667c6c36a327f7ccfff1ef67aba6e352
jgchenu <jgchenu@foxmail.com>
2018-07-24 17:34:44 +0800

    类别列表前端页面

--- service/appApi/goods.js
+++ service/appApi/goods.js
@@ -80,4 +80,65 @@ router.post('/getDetailGoodsInfo', async (ctx) => {
     }
   }
 })
+//读取大类数据的接口
+router.get('/getCategoryList', async (ctx) => {
+  try {
+    const Category = mongoose.model('Category')
+    let result = await Category.find().exec()
+    ctx.body = {
+      code: 200,
+      message: result
+    }
+  } catch (err) {
+    ctx.body = {
+      code: 500,
+      message: err
+    }
+  }
+})
+
+// 读取小类的数据
+router.get('/getCategorySubList', async (ctx) => {
+  try {
+    let categoryId = ctx.request.body.categoryId;
+    categoryId = 1;
+    const CategorySub = mongoose.model('CategorySub')
+    let result = await CategorySub.find({
+      MALL_CATEGORY_ID: categoryId
+    })
+    ctx.body = {
+      code: 200,
+      message: result
+    }
+
+  } catch (err) {
+    ctx.body = {
+      code: 500,
+      message: err
+    }
+  }
+})
+
+// 根据类别获取商品列表
+router.get('/getGoodsListByCategorySubId', async (ctx) => {
+  try {
+    let categorySubId = ctx.request.body.categorySubId;
+    categorySubId = '2c9f6c946016ea9b016016f79c8e0000';
+    const Goods = mongoose.model('Goods')
+    let result = await Goods.find({
+      SUB_ID: categorySubId
+    })
+    ctx.body = {
+      code: 200,
+      message: result
+    }
+
+  } catch (err) {
+    ctx.body = {
+      code: 500,
+      message: err
+    }
+  }
+})
+
 module.exports = router;
---
+++ src/components/pages/CategoryList.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="categoryList">
+        <div class="navBar">
+            <van-nav-bar title="类别列表" left-arrow />
+        </div>
+        <div>
+            <van-row>
+                <van-col span="6">
+                    <div class="leftNav">
+                        左侧导航
+                    </div>
+                </van-col>
+                <van-col span="18">
+                    右侧列表
+                </van-col>
+
+            </van-row>
+        </div>
+    </div>
+</template>
+
+<script>
+import url from "@/serviceAPI.config.js";
+import { Toast } from "vant";
+
+export default {
+  created() {
+    this.getCategory();
+  },
+  methods: {
+    getCategory() {
+      this.$axios({
+        url: url.getCategoryList,
+        method:'get'
+      })
+        .then(res => {
+          console.log(res);
+          if (res.data.code === 200 && res.data.message) {
+          } else {
+            Toast("服务器错误，数据获取失败");
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.categoryList {
+  .leftNav {
+  }
+}
+</style>
+
