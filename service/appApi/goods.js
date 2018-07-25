e11ed915be69b326314b7713c2f87f87052706f2
jgchenu <jgchenu@foxmail.com>
2018-07-25 13:25:35 +0800

    商品列表分页接口完成

--- service/appApi/goods.js
+++ service/appApi/goods.js
@@ -119,14 +119,16 @@ router.post('/getCategorySubList', async (ctx) => {
 })
 
 // 根据类别获取商品列表
-router.get('/getGoodsListByCategorySubId', async (ctx) => {
+router.post('/getGoodsListByCategorySubId', async (ctx) => {
   try {
     let categorySubId = ctx.request.body.categorySubId;
-    categorySubId = '2c9f6c946016ea9b016016f79c8e0000';
+    let page = ctx.request.body.page;
+    let num = 10;
+    let start = (page - 1) * num;
     const Goods = mongoose.model('Goods')
     let result = await Goods.find({
       SUB_ID: categorySubId
-    })
+    }).skip(start).limit(num).exec();
     ctx.body = {
       code: 200,
       message: result
